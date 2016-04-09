136aa3882d494174ab2499d75b16c8dc8152e568
Shagov Alexander <x48j35@gmail.com>
2016-04-09 12:19:15 +0300

    Resolve conflict

--- 1/1.3 sicp.rkt
+++ 1/1.3 sicp.rkt
@@ -1,14 +1,8 @@
 #lang racket
 
-<<<<<<< HEAD
 ; @desc calculates sum of the two largest numbers
 ; @param [int/float]
 ; @return [int/float]
-=======
-;@desc calculates sum of the two largest numbers
-;@param [int/float]
-;@return [int/float]
->>>>>>> 624a010... Update 1.3
 
 (define (square-of-two x y z)
   (+ (square (max (max x y) z)) (square (min (max x y) z))))
