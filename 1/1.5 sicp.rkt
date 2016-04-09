1af3645822988b4c0675b67b255768bc87c29a3c
Shagov Alexander <x48j35@gmail.com>
2016-04-09 15:06:04 +0300

    Add 1.5

---
+++ 1/1.5 sicp.rkt
@@ -0,0 +1,13 @@
+#lang racket
+; @desc test for applicative/normal order evaluation
+; so if it is an applicative order evaluation
+; we can't perform test procedure
+
+(define (p) (p))
+
+(define (test x y)
+  (if (= x 0)
+      0
+      y))
+
+(test 0 (p))
