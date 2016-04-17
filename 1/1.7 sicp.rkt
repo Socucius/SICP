029d3f91c9c0d4b9c90718d732c80351611aafea
Shagov Alexander <x48j35@gmail.com>
2016-04-17 13:35:24 +0300

    Add 1.7

---
+++ 1/1.7 sicp.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+; @descr modificated Newton's method for finding roots
+; @param guess[int/float] first approximation
+; @param x[int/float]     number
+; @return [int/float]     number's root
+
+(define (square-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (square-iter (improve guess x)
+                 x)))
+
+(define (good-enough? guess x)
+  (< (abs (- 1 (/ (* guess guess) x))) 0.000001))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(square-iter 1.0 2.0)
+
